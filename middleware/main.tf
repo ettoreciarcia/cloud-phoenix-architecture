9436845f8b100d89eab894703841e15c364f6eb9
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-01 20:41:14 +0100

    Added alb, elastic ip and alb's security group

--- middleware/main.tf
+++ middleware/main.tf
@@ -21,8 +21,63 @@ module "vpc" {
   external_nat_ip_ids = [aws_eip.nat.id]
 }
 
-
 # Elastic IP
 resource "aws_eip" "nat" {
   vpc = true
 }
+
+resource "aws_eip" "load_balancer" {
+  vpc = true
+}
+resource "aws_security_group" "alb-sg" {
+  name        = "${local.application_name}-${local.environment}-alb-sg"
+  description = "Security group for alb"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb" "alb" {
+  name               = "alb-${local.application_name}-${local.environment}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = [for id in module.vpc.public_subnets : id]
+
+  ip_address_type            = "ipv4"
+  idle_timeout                = 60
+  enable_deletion_protection = false
+
+
+  subnet_mapping {
+    subnet_id     = module.vpc.public_subnets[0]
+    allocation_id = aws_eip.load_balancer.id
+  }
+
+  subnet_mapping {
+    subnet_id     = module.vpc.public_subnets[1]
+    allocation_id = aws_eip.load_balancer.id
+  }
+
+  subnet_mapping {
+    subnet_id     = module.vpc.public_subnets[2]
+    allocation_id = aws_eip.load_balancer.id
+  }
+
+
+  tags = var.tags
+}
+
