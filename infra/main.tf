2e20cad3e51121a3da8e1018aeb5cba4bd7e01ef
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-01 20:14:06 +0100

    Added ecr and task definition

--- infra/main.tf
+++ infra/main.tf
@@ -45,3 +45,59 @@ module "ecs" {
   tags = var.tags
 }
 
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = "ecr-repository-${local.application_name}-${local.environment}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+//ECS TASK DEFINITION
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "${local.application_name}-${local.environment}-app"
+      image     = aws_ecr_repository.ecr_repository.repository_url
+      cpu       = 10
+      memory    = 128
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+}
+
+//ECS SERVICE
+# resource "aws_ecs_service" "mongo" {
+#   name            = "service-${local.application_name}-${local.environment}"
+#   cluster         = module.ecs.cluster_id
+#   task_definition = aws_ecs_task_definition.service.arn
+#   desired_count   = 1
+#   iam_role        = aws_iam_role.foo.arn
+#   depends_on      = [aws_iam_role_policy.foo]
+
+#   ordered_placement_strategy {
+#     type  = "binpack"
+#     field = "cpu"
+#   }
+
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.foo.arn
+#     container_name   = "mongo"
+#     container_port   = 8080
+#   }
+
+#   placement_constraints {
+#     type       = "memberOf"
+#     expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+#   }
+# }
+
