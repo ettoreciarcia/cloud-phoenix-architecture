ca0a73bfc150dd4d9c6f529f19b069d36c0f1e91
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-01 11:34:21 +0100

    Removed module middleware

--- environments/dev/cloudphoenix.auto.tfvars
+++ environments/dev/cloudphoenix.auto.tfvars
@@ -3,7 +3,7 @@ environment      = "dev"
 
 cidr =  "10.100.0.0/16"
 azs  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-region = "eu-central-1"
+region = "eu-west-1"
 public_subnets = ["10.100.10.0/24", "10.100.20.0/24", "10.100.30.0/24"]
 private_subnets = ["10.100.11.0/24", "10.100.21.0/24", "10.100.31.0/24"]
 database_subnets = ["10.100.12.0/24", "10.100.22.0/24", "10.100.32.0/24"]
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,29 +27,16 @@ module "components-cloud-phoenix-architecture" {
   source = "../../infra"
 }
 
-module "middleware" {
-  source = "../../middleware"
+# module "middleware" {
+#   source = "../../middleware"
 
-  name = "VPC-${var.application_name}-${var.environment}"
-  azs = var.azs
-  cidr = var.cidr
-  public_subnets = var.public_subnets
-  private_subnets = var.private_subnets
-  database_subnets = var.database_subnets
-
-  # Tagging/naming resources
-  # public_subnet_suffix = "PublicSubnet"
-  # private_subnet_suffix = "AppPrivateSubnet"
-  # intra_subnet_suffix = "DataPrivateSubnet"
-
-  # Single NAT Gateway
-  # The NAT gateway will be placed in the first public subnet in public_subnets block
-  # enable_nat_gateway = true
-  # single_nat_gateway = true
-  # one_nat_gateway_per_az = false
-  # reuse_nat_ips = true
-  # external_nat_ip_ids = [aws_eip.nat.id]
+#   name = "VPC-${var.application_name}-${var.environment}"
+#   azs = var.azs
+#   cidr = var.cidr
+#   public_subnets = var.public_subnets
+#   private_subnets = var.private_subnets
+#   database_subnets = var.database_subnets
 
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
