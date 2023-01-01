9f9e59da19148376af02bcbe11ccb3d235010f64
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-01 14:06:35 +0100

    Reduced NAT gw number from 3 to 1

--- middleware/main.tf
+++ middleware/main.tf
@@ -13,7 +13,16 @@ module "vpc" {
   public_subnets = var.public_subnets
   private_subnets = var.private_subnets
   database_subnets = var.database_subnets
-  
-  enable_nat_gateway = true
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  reuse_nat_ips = true
+  external_nat_ip_ids = [aws_eip.nat.id]
+}
+
+
+# Elastic IP
+resource "aws_eip" "nat" {
+  vpc = true
 }
