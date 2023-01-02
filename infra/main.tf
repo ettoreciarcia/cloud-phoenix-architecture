604012332b18b7602ae8deed71b6b11fc25c8f4f
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-01-02 12:04:41 +0100

    Fixed port

--- infra/main.tf
+++ infra/main.tf
@@ -71,7 +71,7 @@ resource "aws_ecs_task_definition" "service" {
       portMappings = [
         {
           containerPort = 3000
-          hostPort      = 80
+          hostPort      = 3000
         }
       ]
     }
